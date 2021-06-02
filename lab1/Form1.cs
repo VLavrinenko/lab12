949791790f394a8486c9d6e28ab26f3a5bdb8a1b
Владислав Лавриненко <mitelvlad99sea@gmail.com>
2021-06-02 12:46:04 +0300

    Add project files.

---
+++ lab1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Text = string.Format("Координаты : {0}, {1}", e.X, e.Y);
+            textBox1.Text = (e.X + e.Y).ToString();
+        }
+    }
+}
